5c961e86076baf4e09ff9c9887277b6e776abd30
Vinicius Santana <santanaviny@gmail.com>
2018-10-23 20:41:14 -0700

    enhanements but not done yet

--- js/game-of-life.js
+++ js/game-of-life.js
@@ -13,18 +13,16 @@ const gameOfLife = class gameOfLife {
         this.width = width;
         this.state = Array.apply(null, Array(height)).map(row => Array.apply(null, Array(width)).map(cel => Math.round(Math.random())))
         this.drawMatrix();
-        this.takeStep();
-        // console.log(this.state);
-        // console.log(this.getBrowserInnerDimensions().width, this.getBrowserInnerDimensions().height);
+        document.getElementById('take-step-btn').addEventListener('click', this.takeStep.bind(this));
     }
 
-    countNeighboors(x,y) {
+    countNeighboors(state, x,y) {
         let count = 0;
         for (let row = -1; row <= 1; row++) {
             for (let col = -1; col <= 1; col++) {
                 if (!(row === 0 && col === 0)) {
                     if (x > 0 && x < this.width - 1 && y > 0 && y < this.height - 1) {
-                        if(this.state[x + row][y + col] === 1) {
+                        if(state[x + row][y + col] === 1) {
                             count++;
                         }
                     }                   
@@ -37,6 +35,7 @@ const gameOfLife = class gameOfLife {
     drawMatrix() {
         return new Promise((resolve) => {
             this.el.innerHTML = '';
+            console.log(this.state);
             this.state.map((row, rowIdx) => {
                 const rowWrapper = document.createElement('div');
                 rowWrapper.setAttribute('id', `row-${rowIdx}`);
@@ -52,10 +51,11 @@ const gameOfLife = class gameOfLife {
                         celWrapper.classList.remove("alive");
                         celWrapper.classList.add("dead");
                     }
+                    celWrapper.innerHTML = this.countNeighboors(this.state, rowIdx, colIdx);
                     rowWrapper.appendChild(celWrapper);
                 });
                 this.el.appendChild(rowWrapper);
-            })
+            })            
             resolve();
         })
     }
@@ -65,26 +65,24 @@ const gameOfLife = class gameOfLife {
             const previousState = [].concat(this.state);
             debugger;
             previousState.map((row, rowIdx) => row.map((col, colIdx) => {
-                const celState = previousState[rowIdx, colIdx];
-                const neighboorsAlive = this.countNeighboors(rowIdx, colIdx);
-                if (celState) {
-                    if (neighboorsAlive > 3 || neighboorsAlive < 1) {
-                        this.state[rowIdx][colIdx] = 0;
-                    }
-                } else if (neighboorsAlive >= 3) {
-                        this.state[rowIdx][colIdx] = 1;
+                const celState = previousState[rowIdx][colIdx];
+                const neighboorsAlive = this.countNeighboors(previousState, rowIdx, colIdx);
+                if (celState && (neighboorsAlive >= 3 || neighboorsAlive < 2)) {
+                    this.state[rowIdx][colIdx] = 0;
+                } else if (!celState && neighboorsAlive === 3) {
+                    this.state[rowIdx][colIdx] = 1;
                 } else {
                     this.state[rowIdx][colIdx] = celState;
                 }
+                console.log(`(row, col): ${rowIdx}, ${colIdx} neighB, stateB: ${neighboorsAlive}, ${celState} neighA, stateA: ${this.countNeighboors(this.state, rowIdx, colIdx)}, ${this.state[rowIdx][colIdx]} `);
             }))
+            console.log(this.state);
             resolve();
         })
     }
     
     takeStep() {
-        setInterval(() => {
-            this.updateState().then(this.drawMatrix());
-        }, 2000);
+        this.updateState().then(this.drawMatrix());
     }
 }
 
