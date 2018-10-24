8a05911cc30109bc0d87eba060252c7f3e95aa3c
Vinicius Santana <santanaviny@gmail.com>
2018-10-23 21:48:04 -0700

    Fix: No longer killing alive with 3 neighboors

--- js/game-of-life.js
+++ js/game-of-life.js
@@ -3,18 +3,18 @@ class gameOfLife {
         this.el = document.getElementById(elId);
         this.height = height;
         this.width = width;
-        this.state = Array.apply(null, Array(height)).map(row => Array.apply(null, Array(width)).map(cel => Math.round(Math.random())))
+        this.matrix = Array.apply(null, Array(height)).map(row => Array.apply(null, Array(width)).map(cel => Math.round(Math.random())))
         this.drawMatrix();
         document.getElementById('take-step-btn').addEventListener('click', this.takeStep.bind(this));
     }
 
-    countNeighboors(state, x,y) {
+    countNeighboors(matrix, x,y) {
         let count = 0;
         for (let row = -1; row <= 1; row++) {
             for (let col = -1; col <= 1; col++) {
                 if (!(row === 0 && col === 0)) {
                     if (x > 0 && x < this.width - 1 && y > 0 && y < this.height - 1) {
-                        if(state[x + row][y + col] === 1) {
+                        if(matrix[x + row][y + col] === 1) {
                             count++;
                         }
                     }                   
@@ -27,7 +27,7 @@ class gameOfLife {
     drawMatrix() {
         return new Promise((resolve) => {
             this.el.innerHTML = '';
-            this.state.map((row, rowIdx) => {
+            this.matrix.map((row, rowIdx) => {
                 const rowWrapper = document.createElement('div');
                 rowWrapper.setAttribute('id', `row-${rowIdx}`);
                 rowWrapper.setAttribute('class', `row`);
@@ -35,14 +35,14 @@ class gameOfLife {
                     const celWrapper = document.createElement('div');
                     celWrapper.setAttribute('id', `row-${rowIdx}-col-${colIdx}`)
                     celWrapper.setAttribute('class', `cel`);
-                    if (this.state[rowIdx][colIdx]) {
+                    if (this.matrix[rowIdx][colIdx]) {
                         celWrapper.classList.remove("dead");
                         celWrapper.classList.add("alive");
                     } else {
                         celWrapper.classList.remove("alive");
                         celWrapper.classList.add("dead");
                     }
-                    celWrapper.innerHTML = this.countNeighboors(this.state, rowIdx, colIdx);
+                    celWrapper.innerHTML = this.countNeighboors(this.matrix, rowIdx, colIdx);
                     rowWrapper.appendChild(celWrapper);
                 });
                 this.el.appendChild(rowWrapper);
@@ -51,19 +51,19 @@ class gameOfLife {
         })
     }
 
-    updateState() {
+    updateMatrix() {
         return new Promise((resolve) => {
-            const previousState = [].concat(this.state);
+            const previousState = [].concat(this.matrix);
             debugger;
             previousState.map((row, rowIdx) => row.map((col, colIdx) => {
                 const celState = previousState[rowIdx][colIdx];
                 const neighboorsAlive = this.countNeighboors(previousState, rowIdx, colIdx);
-                if (celState && (neighboorsAlive >= 3 || neighboorsAlive < 2)) {
-                    this.state[rowIdx][colIdx] = 0;
+                if (celState && (neighboorsAlive > 3 || neighboorsAlive < 2)) {
+                    this.matrix[rowIdx][colIdx] = 0;
                 } else if (!celState && neighboorsAlive === 3) {
-                    this.state[rowIdx][colIdx] = 1;
+                    this.matrix[rowIdx][colIdx] = 1;
                 } else {
-                    this.state[rowIdx][colIdx] = celState;
+                    this.matrix[rowIdx][colIdx] = celState;
                 }
             }))
             resolve();
@@ -71,7 +71,7 @@ class gameOfLife {
     }
     
     takeStep() {
-        this.updateState().then(this.drawMatrix());
+        this.updateMatrix().then(this.drawMatrix());
     }
 }
 
--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@ Game of life
 https://www.youtube.com/watch?v=R9Plq-D1gEk
 
 
-# Any live cell with fewer than two live neighbors dies, as if by underpopulation.
-# Any live cell with two or three live neighbors lives on to the next generation.
-# Any live cell with more than three live neighbors dies, as if by overpopulation.
-# Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+- Any live cell with fewer than two live neighbors dies, as if by underpopulation.
+- Any live cell with two or three live neighbors lives on to the next generation.
+- Any live cell with more than three live neighbors dies, as if by overpopulation.
+- Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
